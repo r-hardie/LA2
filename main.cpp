5cd5d45ce26047428b7e67fcd73005f1f80b7aba
Doheonkwak <104535651+Doheonkwak@users.noreply.github.com>
2023-02-03 17:48:15 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,13 +7,20 @@ using namespace std;
 
 int main() {
 	LinkedQueue<int> q1;
-	
+
+
+	for (int i = -100; i < 100; i++) {
+		q1.enqueue(i);
+	}
 	q1.enqueue(1);
 
 	q1.enqueue(2);
 
-	//cout << q1.dequeue();
+	cout << q1.getSize() <<endl;
+
+	cout << q1.dequeue() <<endl;
+	cout << q1.dequeue() <<endl;
 	
-	//cout << q1.getSize();
+	cout << q1.getSize();
 	return 0;
 }
