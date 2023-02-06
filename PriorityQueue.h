4ec52c05727a074bab6d41dbe2b80071198f6183
Doheonkwak <104535651+Doheonkwak@users.noreply.github.com>
2023-02-06 02:17:08 -0800

    Update PriorityQueue.h

--- PriorityQueue.h
+++ PriorityQueue.h
@@ -1,101 +1,24 @@
 #ifndef PRIORITY_QUEUE
 #define PRIORITY_QUEUE
 #include"LinkedQueue.h"
+#include<iostream>
 
-
-template<typename T>
-class PriorityQueue {
-public:
-	PriorityQueue() {
-		for (int i = 0; i < 10; i++) {
-			queues[i] = nullptr;
-		}
-	}
-
-	~PriorityQueue() {
-
-		for (int i = 0; i < 10; i++) {
-			if (queues[i] != nullptr) {
-				delete queues[i];
-			}
-		}
-		/*int findPrio = 0;
-		int findHighIndex = 0;
-		for (int i = 0; i < size; i++) {
-			if (queues[i]->getPrio() > findHighPrio) {
-				findHighPrio = queues[i]->getPrio();//가장 나중에 되어야할 pri 
-				findIndex = i; // 가장나중에 되어야 pri의 인덱스
-			}
-		}
-
-		for (int i = 1; i <= findPrio; i++) {
-			ItemWithPriority<T> nextIndex = 
-		}*/
-	}
-
-	void enqueue(const ItemWithPriority<T>& obj) {
-		ItemWithPriority<T>* itemToInsert = new ItemWithPriority(obj);
-		for (int i = 0; i < 10; i++) {
-			if (queues[i] == nullptr) {
-				queues[i] =
-			}
-		}
-	}
-
-	T dequeue() {
-		if (isEmpty()) {
-			throw runtime_error("Priority Queue is empty");
-		}
-		int highPrio = 0;
-		int index;
-
-		for (int i = 0; i < 10; i++) {
-			if (queues[i]->getprio() > highPrio) {
-				highPrio = queues[i]->getprio();
-				index = i;
-			}
-		}
-
-		T value = queues[index]->value;
-		ItemWithPriority<T>* itemToDelete = queues[index];
-		queues[index] = nullptr;
-		delete itemToDelete;
-
-		return value;
-
-	}
-
-	bool isEmpty() {
-		/*int lowPrio = 10;
-		int lowPrioIndex;
-		for (int i = 0; i < 10; i++) {
-			if (queues[i].getPrio() < lowPrio) {
-				lowPrio = queues[i].getPrio();
-				lowPrioIndex = i;
-			}
-		}
-
-		return queues[lowPrioIndex] = nullptr;*/
-		int count = 0;
-		for (int i = 0; i < 10; i++) {
-			if (queues[i] == nullptr) {
-				count++;
-			}
-		}
-		return count == 10;
-	}
-	
-
-private:
-	LinkedQueue<T>* queues[10];
-};
 
 template<typename T>
 class ItemWithPriority {
 public:
-	ItemWithPriority() : value(0), prio(0) {}
-	ItemWithPriority(T value, int p) : value(value), prio(p) {}
-	
+	ItemWithPriority(){}
+	ItemWithPriority(T v, int p) {
+		value = v;
+		prio = p;
+	}
+	ItemWithPriority(T v) {
+		value = v;
+		prio = 1;
+	}
+	~ItemWithPriority() {
+
+	}
 	T getValue() {
 		return value;
 	}
@@ -111,6 +34,111 @@ public:
 
 private:
 	T value;
+	int prio;
+};
+
+template<typename T>
+class PriorityQueue {
+public:
+	PriorityQueue() {
+		for (int i = 0; i < 10; i++) {
+			queues[i] = nullptr;
+		}
+	}
+	~PriorityQueue() {
+		for (int i = 0; i < 10; i++) {
+			if (queues[i] != nullptr) {
+				delete queues[i];
+			}
+		}
+	}
+
+	void enqueue(const T& obj) {
+		if (isFull()) {
+			throw ("Priority Queue is full");
+		}
+		ItemWithPriority<T>* itemToInsert = new ItemWithPriority<T>(obj);
+		for (int i = 0; i < 10; i++) {
+			if (queues[i] == nullptr) {
+				queues[i] = itemToInsert;
+				break;
+			}
+		}
+		ItemWithPriority<T>* temp;
+		while (true) {
+			bool lFinish = true;
+			for (int i = 0; i < 10; i++) {
+				if(queues[i + 1] == nullptr){
+					break;
+				}
+				if (queues[i]->getPrio() < queues[i + 1]->getPrio()) {
+					temp = queues[i];
+					queues[i] = queues[i + 1];
+					queues[i + 1] = queues[i];
+
+					lFinish = false;
+				}
+			}
+
+			if (lFinish == true) {
+				break;
+			}
+		}
+	}
+
+	T dequeue() {
+		if (isEmpty()) {
+			throw runtime_error("Priority Queue is empty");
+		}
+
+		T value = queues[0]->getValue();
+		
+		ItemWithPriority<T>* itemToDelete = queues[0];
+		for (int i = 0; i < 10; i++) {
+			if (i == 9) {
+				queues[i] = nullptr;
+				break;
+			}
+			if (queues[i + 1] == nullptr) {
+				queues[i] = nullptr;
+				break;
+			}
+
+			queues[i] = queues[i + 1];
+		}
+
+		delete itemToDelete;
+
+		return value;
+
+	}
+
+	bool isEmpty() {
+		int count = 0;
+		for (int i = 0; i < 10; i++) {
+			if (this->queues[i] == nullptr) {
+				count++;
+			}
+		}
+		return count == 10;
+	}
+
+	bool isFull() {
+		int count = 0;
+		for (int i = 0; i < 10; i++) {
+			if (this->queues[i] != nullptr) {
+				count++;
+			}
+		}
+		return count == 10;
+	}
+	
+
+private:
+	//LinkedQueue<T>* queues[10];
+	ItemWithPriority<T>* queues[10];
+};
+#endif
 	int prio;
 };
 
