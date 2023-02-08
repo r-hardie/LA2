05f91378d4411c043dfc91c66d36aa7b3ed2a6b5
Doheonkwak <104535651+Doheonkwak@users.noreply.github.com>
2023-02-07 23:33:23 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include"LinkedQueue.h"
-#include"PriorityQueue.h"
 #include<iostream>
 
 using namespace std;
@@ -14,35 +13,86 @@ void testLinkedQueue() {
 	q1.enqueue(2);
 
 	cout << q1.getSize() << endl;
+	cout << q1.dequeue() << endl;
+	cout << q1.dequeue() << endl;
+	cout << q1.getSize() << endl;
+}
 
-	cout << q1.dequeue() << endl;
-	cout << q1.dequeue() << endl;
-	cout << q1.getSize();
+
+void testPriorityQueue() {
+	PriorityQueue<ItemWithPriority<int>> q;
+
+	ItemWithPriority<int> i1;
+	ItemWithPriority<int> i2;
+	ItemWithPriority<int> i3;
+	ItemWithPriority<int> i4;
+	ItemWithPriority<int> i5;
+	ItemWithPriority<int> i6;
+	ItemWithPriority<int> i7;
+	ItemWithPriority<int> i8;
+	ItemWithPriority<int> i9;
+	ItemWithPriority<int> i10;
+	i1.setValue(1);
+	i1.setPrio(1);
+
+	i2.setValue(10);
+	i2.setPrio(10);
+
+	i3.setValue(9);
+	i3.setPrio(9);
+
+	i4.setValue(8);
+	i4.setPrio(8);
+
+	i5.setValue(7);
+	i5.setPrio(7);
+
+	i6.setValue(6);
+	i6.setPrio(6);
+
+	i7.setValue(4);
+	i7.setPrio(4);
+
+	i8.setValue(5);
+	i8.setPrio(5);
+
+	i9.setValue(3);
+	i9.setPrio(3);
+
+	i10.setValue(2);
+	i10.setPrio(2);
+
+	q.enqueue(i1);
+	q.enqueue(i2);
+	q.enqueue(i3);
+	q.enqueue(i4);
+	q.enqueue(i5);
+	q.enqueue(i6);
+	q.enqueue(i7);
+	q.enqueue(i8);
+	q.enqueue(i9);
+	q.enqueue(i10);
+
+	for (int i = 0; i < 10; i++) {
+		i1 = q.dequeue();
+		cout << i1.getValue() << endl;
+	}
+}
+void testErr() {
+	LinkedQueue<int> q1;
+	q1.dequeue();
 }
 
 int main() {
+	try {
+		testLinkedQueue();
+		testPriorityQueue();
+		testErr();
+	}
+	catch ( runtime_error err) {
+		cout << err.what() << endl;
+	}
+	
 
-	LinkedQueue<int> q1;
-	LinkedQueue<int> q2;
-	ItemWithPriority<LinkedQueue<int>> item1(q1,10);
-	PriorityQueue<ItemWithPriority<LinkedQueue<int>>> pQ;
-	pQ.enqueue(item1);
-	ItemWithPriority<LinkedQueue<int>> nextLinkedQueue = pQ.dequeue();
-	
-	
-	/*LinkedQueue<char> q1;
-	LinkedQueue<char> q2;
-	char c = '1';
-	q1.enqueue(c);
-	ItemWithPriority<LinkedQueue<char>> item1(q1, 10);
-	ItemWithPriority<LinkedQueue<char>> item2(q2, 9);
-	q2.enqueue(4);
-	PriorityQueue<ItemWithPriority<LinkedQueue<int>>> pQ;
-	pQ.enqueue(item2);
-	pQ.enqueue(item1);
-	item2 = pQ.dequeue();
-	q1 = item1.getValue();
-	cout << q1.dequeue();
-	*/
 	return 0;
 }
